385869f2755827c2c777577ac075aabc4841edd1
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-14 12:45:03 +0530

    Polymorphism: Compile Time vs Run Time(Dynamic Method Dispatch)

---
+++ Java_concepts/src/being/rish/polymorphism/Test.java
@@ -0,0 +1,18 @@
+package being.rish.polymorphism;
+
+/*
+Poly + morph => many + form
+Polymorphism is a core concept in object-oriented programming(OOP)
+that allows methods to do different things based on the object it is acting upon,
+even though the method name and its signature might be the same.
+
+Polymorphism types:
+compile time polymorphism - we can achieve compile time polymorphism by method overloading.
+run time(Dynamic method dispatch) polymorphism - we can achieve run time polymorphism by method overriding by the help of inheritance.
+
+
+
+
+*/
+public class Test {
+}
