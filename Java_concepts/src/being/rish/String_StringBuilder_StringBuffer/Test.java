ad18ff0a7c261a3a27213d8e2b6a31994edea2b5
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-14 19:15:58 +0530

    String vs StringBuilder vs StringBuffer

--- Java_concepts/src/being/rish/String_StringBuilder_StringBuffer/Test.java
+++ Java_concepts/src/being/rish/String_StringBuilder_StringBuffer/Test.java
@@ -1,4 +1,21 @@
 package being.rish.String_StringBuilder_StringBuffer;
 
 public class Test {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        StringBuilder sb = new StringBuilder("Hello");
+        // char array ( 16 )
+        // mutable
+        // method chaining
+
+        sb.append(" World").append(" !");
+        String string = sb.toString();
+        System.out.println(string);
+
+        StringBuffer sb1 = new StringBuffer("Hello");
+        // mutable
+        // method chaining
+        // thread safe
+
+    }
 }
