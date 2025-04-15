f3bb8d4b11e8fe988caca3ed121930502f5d731c
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-16 00:08:53 +0530

    Java Generics: Classes, Methods, Interfaces, Enums & Exceptions

--- Java_concepts/src/being/rish/generics/wildcardsInGenerics/Test.java
+++ Java_concepts/src/being/rish/generics/wildcardsInGenerics/Test.java
@@ -1,7 +1,28 @@
 package being.rish.generics.wildcardsInGenerics;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+        List<? super Number> numbers = Arrays.asList(1, 2, 3);
+        numbers.add(12);
+        numbers.add(null);
+    }
 
+    //Upper Boundation
+    public static double sum(List<? extends Number> numbers) {
+        double sum = 0;
+        for(Number o: numbers) {
+            sum += o.doubleValue();
+        }
+        return sum;
+    }
+
+    //Lower Boundation
+    public static void printNumbers(List<? super Integer> list) {
+        for (Object obj: list) {
+            System.out.println(obj);
+        }
     }
 }
